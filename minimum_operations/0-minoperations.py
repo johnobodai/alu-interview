e066b37ca6d71dd888c281214b0dc18874fb2487
johnobodai <j.obodai@alustudent.com>
2023-05-26 21:16:16 +0100

    Added a function to find min operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def minOperations(n):
+    if n == 1:
+        return 0
+
+    num_operations = 0
+    count = 1
+
+    while count < n:
+        if n % count == 0:
+            num_operations += n // count
+            count *n // count
+        else:
+            num_operations += count
+            count += count
